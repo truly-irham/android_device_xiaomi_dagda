f42750c2bccfc470276b6756abd9966780bfcfc9
truly-irham <ep0107566@student.uniten.edu.my>
2024-04-26 00:40:49 +0800

    zeus: Add OTA support for Unofficial builds

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ PRODUCT_PACKAGES += \
     SystemUIResZeus \
     WifiResZeus
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-bliss
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.xiaomi
